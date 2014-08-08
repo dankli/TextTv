6841eca4aed00f939ee66faaed28307ef00fa505
dankli <daniel.klingstedt@gmail.com>
2014-08-08 14:01:12 +0200

    Created a new project named Shared from within Xamarin

---
+++ TextTv.AndroidPhoneApp/Models/Model1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TextTv.AndroidPhoneApp
+{
+	public class Model1
+	{
+		public string Text { get; set; }
+	}
+}
+
---
+++ TextTv.IPhoneApp/Models/Model1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TextTv.IPhoneApp
+{
+	public class Model1
+	{
+		public string Text { get; set; }
+	}
+}
+
