96b656109db98c33c9cc5fee560abb0d7b8d915d
Daniel Klingstedt <daniel.klingstedt@gmail.com>
2014-08-14 15:20:28 +0200

    Moved HtmlParser to Shared project

---
+++ TextTv.Shared/Infrastructure/HtmlParserFactory.cs
@@ -0,0 +1,12 @@
+﻿using TextTv.Shared.Infrastructure.Contracts;
+
+namespace TextTv.Shared.Infrastructure
+{
+    public class HtmlParserFactory : IHtmlParserFactory
+    {
+        public HtmlParser CreateParser(string rawHtml)
+        {
+            return new HtmlParser(new HtmlParserIO(), rawHtml);
+        }
+    }
+}
